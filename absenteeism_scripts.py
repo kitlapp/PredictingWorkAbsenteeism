120916193f7b7685a59836259512a2d5e9990b82
Kimon Lap <kitlappas@gmail.com>
2024-08-04 15:13:42 +0300

    Update Py Scripts File

--- absenteeism_scripts.py
+++ absenteeism_scripts.py
@@ -1,8 +1,5 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 
 
 def range_monthdays(day):
@@ -44,39 +41,64 @@ def range_monthdays(day):
 
 
 def summary_metrics(feature_df, model, x_tr, y_tr, x_te, y_te):
+    """
+        Generates a summary of model metrics including feature weights, intercept, and accuracies.
+
+        Parameters:
+        - feature_df (pd.DataFrame): DataFrame containing feature names.
+        - model (sklearn.linear_model.LogisticRegression): Trained logistic regression model.
+        - x_tr (pd.DataFrame or np.array): Training feature set.
+        - y_tr (pd.Series or np.array): Training target set.
+        - x_te (pd.DataFrame or np.array): Test feature set.
+        - y_te (pd.Series or np.array): Test target set.
+
+        Returns:
+        - pd.DataFrame: DataFrame containing feature weights, odds ratios, intercept, and accuracies.
+        """
+    # Extract feature names and weights from the model:
     weight_names = feature_df.columns.values.tolist()
     weight_values = model.coef_[0].tolist()
-    intercept = model.intercept_[0]  # Extract scalar value directly
-    # Create rows for intercept and accuracy and append them to the DataFrame
+    intercept = model.intercept_[0]
+
+    # Create rows for intercept and accuracy and append them to the DataFrame:
     intercept_row = pd.DataFrame({'Weights & Metrics': ['Intercept'], 'Values': [intercept]})
-    train_accuracy = model.score(x_tr, y_tr)  # Extract scalar value directly
+    train_accuracy = model.score(x_tr, y_tr)
     train_accuracy_row = pd.DataFrame({
         'Weights & Metrics': ['Train Accuracy'], 'Values': [train_accuracy]})
-    test_accuracy = model.score(x_te, y_te)  # Extract scalar value directly
+    test_accuracy = model.score(x_te, y_te)
     test_accuracy_row = pd.DataFrame({
         'Weights & Metrics': ['Test Accuracy'], 'Values': [test_accuracy]})
 
-    # Create DataFrame with feature weights
+    # Create DataFrame with feature weights:
     df = pd.DataFrame({'Weights & Metrics': weight_names, 'Values': weight_values})
 
-    # Calculate the odds ratios
+    # Calculate the odds ratios:
     odds_ratios = np.exp(weight_values)
     df['Odds Ratio'] = odds_ratios
 
-    # Concatenate DataFrames
+    # Concatenate DataFrames:
     df = (pd.concat([
         df, intercept_row, train_accuracy_row, test_accuracy_row])
           .set_index(keys='Weights & Metrics'))
-
-    # Sort the DataFrame by the "Values" column
+    # Sort the DataFrame by the "Odds Ratio" column in descending order:
     df = df.sort_values(by='Odds Ratio', ascending=False)
-
     return df
 
 
 def summary_metrics_on_new_data(new_data_df, predictions, pred_probabilities):
+    """
+        Adds model predictions and probabilities to the new data DataFrame.
+
+        Parameters:
+        - new_data_df (pd.DataFrame): DataFrame containing the new, unseen data.
+        - predictions (np.array): Array of predictions made by the model.
+        - pred_probabilities (np.array): Array of prediction probabilities from the model.
+
+        Returns:
+        - pd.DataFrame: Updated DataFrame with predictions and probabilities added.
+        """
+    # Add the model predictions and probabilities to the new data DataFrame
     new_data_df['Predictions'] = predictions
     new_data_df['Not Extended Absenteeism Probability'] = pred_probabilities[:, 0]
     new_data_df['Extended Absenteeism Probability'] = pred_probabilities[:, 1]
     return new_data_df
-
